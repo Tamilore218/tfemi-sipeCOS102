4d01a737d4d942ea55bd09bb78908d727cba2a5b
Tamilore218 <tamsipe15@gmail.com>
2025-05-09 10:16:51 +0100

    (week_7)

---
+++ week_7/project-2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+data = pd.read_csv("Top-Apps-in-Google-Play.csv")
+df1 = data.iloc[:, :3]
+
+df1
+
+data = pd.read_csv("BTC_DATA_V3.0.csv")
+df2 = data.iloc[:, :3]
+
+df2
+
+data = pd.read_csv("programming language trend over time.csv")
+df3 = data.iloc[:, :3]
+
+df3
